441b4846b02ec95453a8083e85267d2d76507cba
= <=>
2019-03-15 17:50:48 +0200

    include homepage

--- frontend/views.py
+++ frontend/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'frontend/main.html')
+    return render(request, 'frontend/home.html')
 
