85d38186fba3d0bf49d61b21e02423d2066696b4
= <=>
2019-03-29 17:13:33 +0200

    work with menu

--- menu/admin.py
+++ menu/admin.py
@@ -6,10 +6,10 @@ from country.models import Country
 class MenuAdminCreateForm(forms.ModelForm):
     model = Menu
     status = forms.ChoiceField(choices=Menu.LIST_STATUSES)
-    class MenuLang(forms.ModelForm):
-        model = MenuLang
-        name = forms.TextField()
-        lang = forms.ChoiceField(choices=Language.get_list(true)
+    #class Meta:
+        #model = MenuLang
+        #name = forms.TextField()
+        #lang = forms.ChoiceField(choices=Language.get_list(true)
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
